22ae96f87b93f878090521ba8249d0a2137557e0
VictoriaGailitis <105845684+VictoriaGailitis@users.noreply.github.com>
2022-07-03 17:49:55 +0300

    Начальная настройка проекта
    Все файлы проекта для будущей работы с ней + подключение всех необходимых библиотек в main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+from gtts import gTTS
+import playsound
+import os
+import random
+import datetime
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
