ad1238a41112a53c2bd30400d385c000cfd892bc
makarov8889 <makarov1003@mail.ru>
2022-07-03 21:34:12 +0300

    Работа с датасетом
    Добавлен код в main.py и добавлен текст датасета в dialogues.txt

--- main.py
+++ main.py
@@ -7,3 +7,27 @@ import datetime
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 
+def clean_str(r):
+    r = r.lower()
+    r = [c for c in r if c in alphabet]
+    return ''.join(r)
+
+alphabet = ' 1234567890-йцукенгшщзхъфывапролджэячсмитьбюёqwertyuiopasdfghjklzxcvbnm'
+
+with open('dialogues.txt', encoding='utf-8') as f:
+    content = f.read()
+
+blocks = content.split('\n')
+dataset = []
+for block in blocks:
+    replicas = block.split('\\')[:2]
+    if len(replicas) == 2:
+        pair = [clean_str(replicas[0]), clean_str(replicas[1])]
+        if pair[0] and pair[1]:
+            dataset.append(pair)
+
+X_text = []
+y = []
+for question, answer in dataset[:10000]:
+    X_text.append(question)
+    y += [answer]
